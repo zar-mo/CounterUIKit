10ccad0d46b29060a5a40877cb730e8507a1a69a
Abouzar <abouzarmoradian1@gmail.com>
2024-08-29 17:21:07 -0400

    added a new scroll for posts

---
+++ CounterUIKit/PostTableVC.swift
@@ -0,0 +1,55 @@
+//
+//  PostTableVC.swift
+//  CounterUIKit
+//
+//  Created by Abouzar Moradian on 8/29/24.
+//
+
+import UIKit
+
+class PostTableVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+   
+    let posts = [
+        (title: "Beautiful Sunset", image: UIImage(named: "sunset.jpg"), caption: "A beautiful sunset at the beach."),
+        (title: "City Skyline", image: UIImage(named: "city.jpg"), caption: "The city skyline looks amazing at night."),
+        (title: "Mountain Trek", image: UIImage(named: "mountain.jpg"), caption: "Hiking the mountains is so refreshing.")
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as? PostTableViewCell else {
+            fatalError("The dequeued cell is not an instance of PostTableViewCell.")
+        }
+
+      
+        let post = posts[indexPath.row]
+        
+      
+        cell.titleLabel.text = post.title
+        cell.postImageView.image = post.image
+        cell.captionLabel.text = post.caption
+
+        return cell
+    }
+
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
+}
+
---
+++ CounterUIKit/PostTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostTableViewCell.swift
+//  CounterUIKit
+//
+//  Created by Abouzar Moradian on 8/29/24.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
+
